27e1f0ccbc8f7deed7dc389ae0f8dae2e4359e9b
Miguel Salazar <migueljsalazar@icloud.com>
2015-11-13 15:03:53 -0500

    Update siskel.rb
    Updated this file with completed siskel movie finder that passes all tests.

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,29 @@
 require 'httparty'
 
 class Siskel
+  attr_reader :rating
+  attr_reader :year
+  attr_reader :plot
   attr_reader :title
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
-    @title = @movie['Title']
+  def initialize(title, opts = {} )
+    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{opts[:year]}&plot=#{opts[:plot]}&tomatoes=true")
+    @rating = @movie['Rated']
+    @year = @movie['Year']
+    @plot = @movie['Plot']
+    @title = @movie['Title'] || @movie['Error']
   end
-end
+
+  def consensus
+    rating = @movie['tomatoMeter'].to_i
+    if ( rating < 25 )
+      "2 Thumbs Down"
+    elsif (rating <50)
+      "Thumbs Down"
+    elsif (rating < 75)
+      "Thumbs Up"
+    else
+      "2 Thumbs Up"
+    end
+  end
+end 
